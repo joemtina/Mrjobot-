0d45eb34871f2d931bdc0966243d3d52654bde68
joemtina <joemtina@gmail.com>
2025-06-03 13:24:12 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: '‎Bot',
   botName: "Knight Bot",
   botOwner: 'Mr JOE', // Your name
   ownerNumber: '255612695686', //Set your number here without + symbol, just add country code & number without any space
